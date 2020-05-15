b37067beb147896f861dec4809386c098fb4d1fa
Speretta <ismailpolat322735@gmail.com>
2020-05-16 02:50:40 +0300

    [Edited] plugin.yml

--- SenMisin/src/me/Speretta/SenMisin/Event.java
+++ SenMisin/src/me/Speretta/SenMisin/Event.java
@@ -5,8 +5,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerLoginEvent;
-import org.bukkit.event.player.PlayerLoginEvent.Result;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import me.Speretta.SenMisin.GirisAPI.API;
